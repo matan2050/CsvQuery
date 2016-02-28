a28d6e27c22cba71e5d03b34923f0016402c3f41
Matan <matan2050@gmail.com>
2016-02-28 06:38:37 +0200

    initial commit, works but needs some testing and refactoring

---
+++ SearchInFile_InterviewQuestion_1/CsvIndexer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SearchInFile_InterviewQuestion_1
+{
+    public class CsvIndexer
+    {
+        private string csvPath;
+        private Dictionary<string, List<long>> emailIndices;
+        private Dictionary<string, List<long>> nameIndices;
+
+        public CsvIndexer(string _csvPath)
+        {
+            csvPath = _csvPath;
+
+            if (!File.Exists(csvPath))
+            {
+                throw new FileNotFoundException("Csv Path not valid");
+            }
+        }
+
+
+        public void Index()
+        {
+            string[] lines = File.ReadAllLines(csvPath);
+
+            // Defining dictionaries becuase now we have an upper bound on number of unique records in csv
+            emailIndices = new Dictionary<string, List<long>>((int)lines.Length);
+            nameIndices = new Dictionary<string, List<long>>((int)lines.Length);
+
+            foreach (string line in lines)
+            {
+                long currId;
+
+                string[] lineSplit = line.Split(',');
+                long.TryParse(lineSplit[0], out currId);
+
+                string currName = lineSplit[1];
+
+                if (currName[0] == ' ')
+                {
+                    currName = currName.Substring(1, (currName.Length - 1));
+                }
+                string currEmail = lineSplit[2].Trim();
+                
+                // Adding new index to current email
+                if (!emailIndices.ContainsKey(currEmail))
+                {
+                    emailIndices.Add(currEmail, new List<long>());
+                }
+                emailIndices[currEmail].Add(currId);
+
+                // Adding new index to current name
+                if (!nameIndices.ContainsKey(currName))
+                {
+                    nameIndices.Add(currName, new List<long>());
+                }
+                nameIndices[currName].Add(currId);
+            }
+        }
+
+
+        public List<long> ProcessQuery(string query)
+        {
+            List<long> matchingIndices = new List<long>();
+
+            if (query.Length == 0)
+            {
+                throw new Exception("Query is empty");
+            }
+
+            if (query.Contains("@"))
+            {
+                if (emailIndices.ContainsKey(query))
+                {
+                    matchingIndices = emailIndices[query];
+                }
+            }
+            else
+            {
+                if (nameIndices.ContainsKey(query))
+                {
+                    matchingIndices = nameIndices[query];
+                }
+            }
+        
+            return matchingIndices;
+        }
+    }
+}
---
+++ SearchInFile_InterviewQuestion_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInFile_InterviewQuestion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // Get number of logical processors, for future multithreading
+            Console.WriteLine("Available cores {0}", Environment.ProcessorCount);
+            string path = @"C:\Users\User\Desktop\InterviewQuestions\simulation.csv";
+            CsvIndexer csvToQuery = new CsvIndexer(path);
+            csvToQuery.Index();
+
+            Console.WriteLine("Enter email or name for querying:");
+            string query;
+
+            bool keepQuerying = true;
+
+            while (keepQuerying)
+            {
+                query = Console.ReadLine();
+
+                if (query.Length == 0)
+                {
+                    keepQuerying = false;
+                }
+                else
+                {
+                    List<long> queryResult = csvToQuery.ProcessQuery(query);
+
+                    if (queryResult.Count == 0)
+                    {
+                        Console.WriteLine("Query returned 0 matches");
+                    }
+                    else
+                    {
+                        string printedResult = "";
+                        for (int i = 0; i < queryResult.Count; i++)
+                        {
+                            printedResult += queryResult[i].ToString();
+
+                            if (i != queryResult.Count - 1)
+                            {
+                                printedResult += ", ";
+                            }
+                        }
+
+                        Console.WriteLine(printedResult);
+                    }
+                }
+            }          
+        }
+    }
+}
