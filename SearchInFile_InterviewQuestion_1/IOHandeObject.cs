771be4edda52d3c3125a6984b637a41fa436e50a
Matan <matan2050@gmail.com>
2016-03-05 10:04:01 +0200

    Added class to handle IO actions and encapsulate program flow, rather than all code in program.cs

---
+++ SearchInFile_InterviewQuestion_1/IOHandeObject.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SearchInFile_InterviewQuestion_1
+{
+    public class IOHandeObject
+    {
+        #region FIELDS
+        private string      pathToCsv;
+        private bool        keepQuerying;
+        private bool        timeOperations;
+        Stopwatch           timer;
+        TimeSpan            timeToInit;
+        CsvIndexer          csvToQuery;
+        #endregion
+
+
+        #region CONSTRUCTORS
+        public IOHandeObject(string[] inputArgs)
+        {
+            timeOperations = false;
+            keepQuerying = true;
+
+            Console.Clear();
+
+            // Check input argument
+            if (inputArgs.Length == 0)
+            {
+                Console.WriteLine("Missing Csv Path");
+                return;
+            }
+
+            pathToCsv = inputArgs[0];
+
+            if (inputArgs.Length == 2)
+            {
+                if (inputArgs[1] == "/t")
+                {
+                    timeOperations = true;
+                    Console.WriteLine("Timing load and query operations");
+                }
+            }
+
+            if (timeOperations)
+            {
+                timer = new Stopwatch();
+                timer.Start();
+            }
+
+            csvToQuery = new CsvIndexer(pathToCsv);
+            csvToQuery.Index();
+
+            if (timeOperations)
+            {
+                timer.Stop();
+                timeToInit = timer.Elapsed;
+                timer.Reset();
+                Console.WriteLine("Time to load csv: {0}", timeToInit.ToString());
+            }
+        }
+        #endregion
+
+
+        #region PUBLIC_METHODS
+        public void ProcessQueries()
+        {
+            Console.WriteLine("Enter email or name for querying:");
+
+            string      query;
+            TimeSpan    timeToQuery;
+
+            while (keepQuerying)
+            {
+                query = Console.ReadLine();
+
+                if (query == "exit")
+                {
+                    keepQuerying = false;
+                    break;
+                }
+
+                if (timeOperations)
+                {
+                    timer.Start();
+                }
+                
+                List<long> queryResult = csvToQuery.ProcessQuery(query);
+
+                if (timeOperations)
+                {
+                    timer.Stop();
+                    timeToQuery = timer.Elapsed;
+                    Console.WriteLine("Time to query: {0}", timeToQuery.ToString());
+                }
+
+                string printedResult = CreateResponseString(ref queryResult);
+                Console.WriteLine(printedResult);
+                
+            }
+        }
+        #endregion
+
+
+        #region PRIVATE_METHODS
+        private string CreateResponseString(ref List<long> queryResults)
+        {
+            string printedResult = "";
+
+            if (queryResults.Count == 0)
+            {
+                printedResult = "Query returned 0 matches";
+            }
+            else
+            {
+                for (int i = 0; i < queryResults.Count; i++)
+                {
+                    printedResult += queryResults[i].ToString();
+
+                    if (i != queryResults.Count - 1)
+                    {
+                        printedResult += ", ";
+                    }
+                }
+            }
+
+            return printedResult;
+        }
+        #endregion
+    }
+}
--- SearchInFile_InterviewQuestion_1/Program.cs
+++ SearchInFile_InterviewQuestion_1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
 
 namespace SearchInFile_InterviewQuestion_1
@@ -11,78 +8,83 @@ namespace SearchInFile_InterviewQuestion_1
     {
         static void Main(string[] args)
         {
-            Console.Clear();
+            //         Console.Clear();
 
-			// Check input argument
-			if (args.Length == 0)
-			{
-				Console.WriteLine("Missing Csv Path");
-				return;
-			}
+            //// Check input argument
+            //if (args.Length == 0)
+            //{
+            //	Console.WriteLine("Missing Csv Path");
+            //	return;
+            //}
 
-			string path = @args[0];
+            //string path = @args[0];
 
+            //         // Get number of logical processors, for future multithreading
+            //         Console.WriteLine("Available cores {0}", Environment.ProcessorCount);
 
-            // Get number of logical processors, for future multithreading
-            Console.WriteLine("Available cores {0}", Environment.ProcessorCount);
-            //string path = @"C:\Temp\simulationCsv - Copy.csv";
+            //Stopwatch   timer       = new Stopwatch();
+            //         CsvIndexer  csvToQuery  = new CsvIndexer(path);
 
-			Stopwatch timer = new Stopwatch();
-			timer.Start();
-            CsvIndexer csvToQuery = new CsvIndexer(path);
-            csvToQuery.Index();
-			timer.Stop();
+            //         // Timed process
+            //         timer.Start();
+            //         csvToQuery.Index();
+            //timer.Stop();
 
-			TimeSpan timeToInit = timer.Elapsed;
+            //TimeSpan    timeToInit  = timer.Elapsed;
 
-			timer.Reset();
+            //timer.Reset();
 
-			Console.WriteLine("Time to load csv: {0}", timeToInit.ToString());
-            Console.WriteLine("Enter email or name for querying:");
-            string query;
+            //Console.WriteLine("Time to load csv: {0}", timeToInit.ToString());
+            //         Console.WriteLine("Enter email or name for querying:");
 
-            bool keepQuerying = true;
+            //         string  query;
+            //         bool    keepQuerying    = true;
 
-            while (keepQuerying)
-            {
-                query = Console.ReadLine();
+            //         while (keepQuerying)
+            //         {
+            //             query = Console.ReadLine();
 
-                if (query.Length == 0)
-                {
-                    keepQuerying = false;
-                }
-                else
-                {
-					timer.Start();
-                    List<long> queryResult = csvToQuery.ProcessQuery(query);
+            //             if (query.Length == 0)
+            //             {
+            //                 keepQuerying = false;
+            //             }
+            //             else
+            //             {
+            //		timer.Start();
+            //                 List<long>  queryResult     = csvToQuery.ProcessQuery(query);
+            //		timer.Stop();
+            //		TimeSpan    timeToQuery     = timer.Elapsed;
 
-					timer.Stop();
-					TimeSpan timeToQuery = timer.Elapsed;
+            //		string printedResult = "";
 
-					string printedResult = "";
+            //		if (queryResult.Count == 0)
+            //                 {
+            //			printedResult = "Query returned 0 matches";
+            //                 }
+            //                 else
+            //                 {
 
-					if (queryResult.Count == 0)
-                    {
-						printedResult = "Query returned 0 matches";
-                    }
-                    else
-                    {
-                        
-                        for (int i = 0; i < queryResult.Count; i++)
-                        {
-                            printedResult += queryResult[i].ToString();
+            //                     for (int i = 0; i < queryResult.Count; i++)
+            //                     {
+            //                         printedResult += queryResult[i].ToString();
 
-                            if (i != queryResult.Count - 1)
-                            {
-                                printedResult += ", ";
-                            }
-                        }
-                    }
+            //                         if (i != queryResult.Count - 1)
+            //                         {
+            //                             printedResult += ", ";
+            //                         }
+            //                     }
+            //                 }
 
-					Console.WriteLine(printedResult);
-					Console.WriteLine("Time to query: {0}", timeToQuery.ToString());
-				}
-            }          
+            //		Console.WriteLine(printedResult);
+            //		Console.WriteLine("Time to query: {0}", timeToQuery.ToString());
+            //	}
+            //         }  
+
+            args = new string[2];
+            args[0] = @"C:\Users\User\Desktop\InterviewQuestions\simulation.csv";
+            args[1] = "/t";
+            IOHandeObject runInstance = new IOHandeObject(args);
+            runInstance.ProcessQueries();        
         }
     }
 }
